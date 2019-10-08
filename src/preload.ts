5f114699be23802461cbb19a3b46aeaa285f5619
Tundra Fizz <coleman.leif@gmail.com>
2019-10-07 20:02:30 -0700

    Update files

---
+++ src/preload.ts
@@ -0,0 +1,12 @@
+console.log("It's working!: TypeScript");
+window.preloadWasRun = "Preload was run for TypeScript";
+
+// import {remote} from "electron";
+
+// let currentWindow = remote.BrowserWindow.getFocusedWindow();
+
+// console.log("preload");
+
+// window.closeCurrentWindow = function(){
+//   currentWindow.close();
+// }
